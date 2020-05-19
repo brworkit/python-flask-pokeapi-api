81fce4f0cd3c89df3887acdc6e845fa311fe5781
brworkit <brworkit@gmail.com>
2020-05-19 01:32:10 -0300

    assets

--- application/controllers/pokemons/assets_resource.py
+++ application/controllers/pokemons/assets_resource.py
@@ -1,7 +1,5 @@
-from os import environ
 from http import HTTPStatus
-from flask import Blueprint, jsonify, request
-from application.utils import time, log
+from flask import Blueprint, jsonify
 from application.services.pokemon_service import PokemonService
 
 blueprint = Blueprint('assets_resource', __name__)
@@ -9,10 +7,6 @@ pokemon_service = PokemonService()
 
 @blueprint.route("/v1/assets/<id>", methods=["GET"])
 def find_assets(id):
-    print("find_assets")
-    print(f"id: {id}") 
-
-    result = pokemon_service.assets(id)
-        
+    result = pokemon_service.assets(id)        
     return jsonify({"result": result}), HTTPStatus.OK
     
